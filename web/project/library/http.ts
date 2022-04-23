6a6f2cccbedb39e9be21ed26aa179c0957034ee3
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2022-04-23 06:12:42 +0000

    feature: 開発環境でのHTTPリクエストをSSRに対応

--- web/project/library/http.ts
+++ web/project/library/http.ts
@@ -1,14 +1,29 @@
 import axios from 'axios';
-axios.defaults.headers.common['Content-Type'] = 'Application/json';
+const instance = axios.create({
+  baseURL:
+    typeof window === 'undefined'
+      ? 'http://localhost:3000'
+      : 'http://localhost:53000',
+});
+instance.defaults.headers.common['Content-Type'] = 'Application/json';
 
 const post = async (url: string, data?: any) => {
   try {
-    return await axios.post(url, data);
+    return await instance.post(url, data);
   } catch (err: any) {
-    throw err.response;
+    throw err.response ?? err;
+  }
+};
+
+const get = async (url: string) => {
+  try {
+    return await instance.get(url);
+  } catch (err: any) {
+    throw err.response ?? err;
   }
 };
 
 export const http = {
   post,
+  get,
 };
